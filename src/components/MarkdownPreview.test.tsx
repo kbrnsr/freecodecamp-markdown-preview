9f7e8ca4c0f7ba6723074ca1fd28811a24ebbe37
Kabeer Nasir <1452229+kbrnsr@users.noreply.github.com>
2022-07-31 15:29:46 +0200

    move tests to it's own describe block

--- src/components/MarkdownPreview.test.tsx
+++ src/components/MarkdownPreview.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { screen, render } from '@testing-library/react';
+import { text } from 'node:stream/consumers';
 import MarkdownPreview from './MarkdownPreview';
 import constants from '../constants';
 
@@ -8,26 +9,30 @@ const {
   mpPreview,
 } = constants;
 
-beforeEach(() => {
-  render(<MarkdownPreview />);
-});
-test('component wrapper existence', () => {
-  const wrapperElement = screen.getByTestId(mpWrapper);
-  expect(wrapperElement).toBeInTheDocument();
-});
-test('preview box existence', () => {
-  const mpEditorwBoxElement = screen.getByTestId(mpEditorBox);
-  expect(mpEditorwBoxElement).toBeInTheDocument();
-});
+describe('structure tests', () => {
+  beforeEach(() => {
+    render(<MarkdownPreview />);
+  });
 
-test('editor element existence', () => {
-  const mpEditorElement = screen.getByRole('textbox');
-  expect(mpEditorElement)
-    .toHaveAttribute('data-testid', expect
-      .stringContaining(mpEditor));
-});
+  test('component wrapper existence', () => {
+    const wrapperElement = screen.getByTestId(mpWrapper);
+    expect(wrapperElement).toBeInTheDocument();
+  });
 
-test('preview element existence', () => {
-  const mpPreviewElement = screen.getByTestId(mpPreview);
-  expect(mpPreviewElement).toBeInTheDocument();
+  test('preview box existence', () => {
+    const mpEditorwBoxElement = screen.getByTestId(mpEditorBox);
+    expect(mpEditorwBoxElement).toBeInTheDocument();
+  });
+
+  test('editor element existence', () => {
+    const mpEditorElement = screen.getByRole('textbox');
+    expect(mpEditorElement)
+      .toHaveAttribute('data-testid', expect
+        .stringContaining(mpEditor));
+  });
+
+  test('preview element existence', () => {
+    const mpPreviewElement = screen.getByTestId(mpPreview);
+    expect(mpPreviewElement).toBeInTheDocument();
+  });
 });
