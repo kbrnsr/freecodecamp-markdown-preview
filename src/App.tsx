007a63f07aaa8f45c0944f998ca0e78803ce55ff
Kabeer Nasir <1452229+kbrnsr@users.noreply.github.com>
2022-08-01 15:04:51 +0200

    remove boiler plate code from App 	modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import MarkdownPreview from './components/MarkdownPreview';
 import './App.css';
 
@@ -7,24 +6,6 @@ function App() {
   return (
     <div className="App">
       <MarkdownPreview />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.tsx</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
