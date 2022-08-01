7dcc439bfd8afb293bfc12866d726f4e5b2885f7
Kabeer Nasir <1452229+kbrnsr@users.noreply.github.com>
2022-08-01 08:40:51 +0200

    test preview mirrors editor

--- src/components/MarkdownPreview.test.tsx
+++ src/components/MarkdownPreview.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import MarkdownPreview from './MarkdownPreview';
 import constants from '../constants';
 
@@ -7,6 +8,22 @@ const {
   mpWrapper, mpEditorBox, mpEditor,
   mpPreviewBox, mpPreview,
 } = constants;
+
+describe('functionality tests', () => {
+  test('preview mirrors editor', async () => {
+    render(<MarkdownPreview />);
+    const user = userEvent.setup();
+    const testString = 'What is love?\n'
+    + '\tBaby, don\'t hurt me\n\n'
+    + 'no more';
+
+    const mpEditorElement = screen.getByRole('textbox');
+    const mpPreviewElement = screen.getByTestId(mpPreview);
+
+    await user.type(mpEditorElement, testString);
+    expect(mpPreviewElement.innerHTML).toBe(testString);
+  });
+});
 
 describe('structure tests', () => {
   beforeEach(() => {
